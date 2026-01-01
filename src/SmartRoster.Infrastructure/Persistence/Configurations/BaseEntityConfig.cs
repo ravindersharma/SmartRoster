9c42f974b4942bddca85954360572d370b54cce5
ravindersharma <ravindersharma583@gmail.com>
2026-01-01 23:34:51 +0530

    feat(infrastructure):Add BaseConfig

---
+++ src/SmartRoster.Infrastructure/Persistence/Configurations/BaseEntityConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartRoster.Domain.Entities.Base;
+
+namespace SmartRoster.Infrastructure.Persistence.Configurations;
+
+/// <summary>
+/// Global configuration for all entities inheriting BaseEntity.
+/// </summary>
+public abstract class BaseEntityConfig<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+{
+
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).ValueGeneratedNever();
+        builder.Property(x => x.CreatedAt).IsRequired();
+        builder.Property(x => x.UpdatedAt).IsRequired(false);
+    }
+}
--- src/SmartRoster.Infrastructure/Persistence/Configurations/EmployeConfig.cs
+++ src/SmartRoster.Infrastructure/Persistence/Configurations/EmployeConfig.cs
@@ -5,23 +5,23 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SmartRoster.Infrastructure.Persistence.Configurations
+namespace SmartRoster.Infrastructure.Persistence.Configurations;
+
+/// <summary>
+/// EF configuration for Employee entity.
+/// </summary>
+public class EmployeConfig : BaseEntityConfig<Employee>
 {
-    public class EmployeConfig : IEntityTypeConfiguration<Employee>
+    public override void Configure(EntityTypeBuilder<Employee> builder)
     {
-        public void Configure(EntityTypeBuilder<Employee> builder)
-        {
-            builder.HasKey(p => p.Id);
+        base.Configure(builder);
+        builder.Property(p => p.FirstName).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.LastName).IsRequired().HasMaxLength(50);
+        builder.Property(p => p.Email).IsRequired().HasMaxLength(100);
+        builder.Property(p => p.Mobile).IsRequired().HasMaxLength(15);
+        builder.Property(p => p.Department).IsRequired().HasMaxLength(100);
 
 
-            builder.Property(p => p.FirstName).IsRequired().HasMaxLength(50);
-            builder.Property(p => p.LastName).IsRequired().HasMaxLength(50);
-            builder.Property(p => p.Email).IsRequired().HasMaxLength(100);
-            builder.Property(p => p.Mobile).IsRequired().HasMaxLength(15);
-            builder.Property(p => p.Department).IsRequired().HasMaxLength(100);
-
-
-            builder.HasIndex(p => p.Email).IsUnique();
-        }
+        builder.HasIndex(p => p.Email).IsUnique();
     }
 }
